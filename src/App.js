840c20ccc7a70decbd7c2c4146ff8f8cb3ce3098
Fernando Carrillo <fernandoh.cabe@gmail.com>
2022-07-15 22:27:46 -0500

    welcome to the git workshop

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './img/logo.jpg';
 import './App.css';
 
 function App() {
@@ -7,15 +7,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to the Github workshop.
         </p>
+        <span className='hideThis'> Where hopes and dreams go missing.</span>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://cdn.wamiz.fr/cdn-cgi/image/quality=80,width=400,height=200,fit=cover/article/main-picture/628b45168290d358908353.jpg"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Github here
         </a>
       </header>
     </div>
