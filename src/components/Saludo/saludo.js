abd8fdb76368d04dfa3ef45abb0b688b40b10b59
Fernando Carrillo <fernandoh.cabe@gmail.com>
2022-07-16 23:21:50 -0500

    sin conflictos

--- src/components/Saludo/saludo.js
+++ src/components/Saludo/saludo.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function saludo() {
   return (
-    <div>saludos, mi nombre es Cesar</div>
+    <div>
+      saludos, mi nombre es Cesar
+      <h1>Hola mundo</h1>
+      <h2>Espero no tener conflictos</h2>
+    </div>
   )
 }
